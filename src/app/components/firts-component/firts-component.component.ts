d0274e51914a96b9208c890bace43f2226224465
Givaldo Fernandes <122561742+Gilferprog@users.noreply.github.com>
2023-02-15 21:48:42 -0300

    aula07

--- src/app/components/firts-component/firts-component.component.ts
+++ src/app/components/firts-component/firts-component.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./firts-component.component.css']
 })
 export class FirtsComponentComponent {
+  name: string ='Givaldo';
+  age: number = 61
+  job: string ='Programador';
 
 }
